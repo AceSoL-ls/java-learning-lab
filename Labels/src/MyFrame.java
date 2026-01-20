bcb835299a1f9b09052cae34cd286a3dc6f79fb8
AceSoL-ls <eyris97@gmail.com>
2026-01-20 14:36:51 +0200

    new small update

--- Labels/src/MyFrame.java
+++ Labels/src/MyFrame.java
@@ -4,20 +4,23 @@ import java.awt.*;
 public class MyFrame extends JFrame  {
 
     JLabel label = new JLabel();
-    ImageIcon image = new ImageIcon("addict.jpg");
+    ImageIcon image = new ImageIcon("coder.jpg");
 
     MyFrame(){
         this.setTitle("Hey whats up world!");
         this.setSize(500,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
 
         label.setText("I like coding a lot");
         label.setIcon(image);
-        label.setBounds(150,200,200,30);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setHorizontalTextPosition(JLabel.CENTER);
         label.setForeground(new Color(0x00FF00));
         label.setBackground(Color.magenta);
-        label.setFont(new Font("MV Boli",Font.PLAIN,20));
+        label.setOpaque(true);
+        label.setFont(new Font("MV Boli",Font.PLAIN,24));
 
         this.add(label);
         this.setVisible(true);
