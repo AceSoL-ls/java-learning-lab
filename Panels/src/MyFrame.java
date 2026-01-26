a2a07ae1747b191accf9c5fbe440e620bbc0c1aa
AceSoL-ls <eyris97@gmail.com>
2026-01-26 22:43:02 +0200

    New Panel training

--- Panels/src/MyFrame.java
+++ Panels/src/MyFrame.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class MyFrame extends JFrame {
 
-    ImageIcon image = new ImageIcon();
+    ImageIcon icon = new ImageIcon("Emoji.png");
 
     JLabel label = new JLabel();
 
@@ -14,15 +14,22 @@ public class MyFrame extends JFrame {
     MyFrame(){
 
         label.setText("Cool Text Bro!");
+        label.setIcon(icon);
+        label.setVerticalAlignment(JLabel.BOTTOM);
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setBounds(50, 120, 90, 120);
 
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0, 250, 250);
+        redPanel.setLayout(null);
 
         bluePanel.setBackground(Color.blue);
         bluePanel.setBounds(250, 0, 250, 250);
+        bluePanel.setLayout(null);
 
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0, 250, 500 ,250);
+        greenPanel.setLayout(null);
 
         this.setTitle("My New Panel!");
         this.setSize(750,750);
@@ -32,6 +39,7 @@ public class MyFrame extends JFrame {
 
         this.add(label);
 
+        bluePanel.add(label);
         this.add(redPanel);
         this.add(bluePanel);
         this.add(greenPanel);
