f8e9a6e2ea40711f973479d5d02beefb068ec98c
AceSoL-ls <eyris97@gmail.com>
2026-02-09 01:08:35 +0200

    Swing updates

--- Buttons/src/MyFrame.java
+++ Buttons/src/MyFrame.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame  {
+public class MyFrame extends JFrame implements ActionListener{
 
     JButton button;
     JLabel label;
@@ -16,6 +16,7 @@ public class MyFrame extends JFrame  {
         label = new JLabel();
         label.setIcon(icon2);
         label.setBounds(150, 250, 150, 150);
+        label.setVisible(false);
 
         button = new JButton();
         button.setBounds(100, 100, 250, 100);
@@ -36,17 +37,18 @@ public class MyFrame extends JFrame  {
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setSize(600, 600);
+        this.setSize(500, 500);
         this.setVisible(true);
         this.add(button);
         this.add(label);
     }
-    /*
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button) {
-            System.out.println("Bro thats craaz");
-        } */
-
+        if (e.getSource() == button) {
+            //System.out.println("Bro thats craaz");
+            label.setVisible(true);
+        }
+    }
 
 }
