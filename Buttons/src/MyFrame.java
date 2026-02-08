890d7a5e1d1bbd14dd48ecd61a0407867b158de5
AceSoL-ls <eyris97@gmail.com>
2026-02-08 21:34:25 +0200

    Swing updates

--- Buttons/src/MyFrame.java
+++ Buttons/src/MyFrame.java
@@ -3,9 +3,14 @@ import java.awt.*;
 
 public class MyFrame extends JFrame {
     public MyFrame() throws HeadlessException {
+
+        JButton button = new JButton();
+        button.setBounds(200, 100, 100, 50);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
+        this.add(button);
     }
 }
