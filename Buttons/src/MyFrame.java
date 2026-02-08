8d54c309ffa7dff620562e453eff39c96b1e6668
AceSoL-ls <eyris97@gmail.com>
2026-02-08 21:37:14 +0200

    Swing updates

--- Buttons/src/MyFrame.java
+++ Buttons/src/MyFrame.java
@@ -4,9 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button = new JButton();
+
     public MyFrame() throws HeadlessException {
 
-        JButton button = new JButton();
         button.setBounds(200, 100, 100, 50);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,6 +20,9 @@ public class MyFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button) {
+            System.out.println("Bro thats craaz");
+        }
 
     }
 }
