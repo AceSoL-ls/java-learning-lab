18819209edbe9414a2cd21f9fc4a3a8fd985756c
AceSoL-ls <eyris97@gmail.com>
2026-02-10 22:21:33 +0200

    Swing updates

--- BorderLayout/src/MyFrame.java
+++ BorderLayout/src/MyFrame.java
@@ -1,10 +1,19 @@
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
+
+    JPanel panel = new JPanel();
+
     MyFrame(){
         this.setSize(600,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
     }
 }
