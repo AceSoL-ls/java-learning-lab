3995f11c68513a37b55069e24cf8d734bae52041
AceSoL-ls <eyris97@gmail.com>
2026-02-13 20:33:27 +0200

    Swing updates

--- GridLayout/src/MyFrame.java
+++ GridLayout/src/MyFrame.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
    public MyFrame(){
        this.setSize(500,500);
-       
+       this.setLayout(new GridLayout(3,3));
     }
 }
