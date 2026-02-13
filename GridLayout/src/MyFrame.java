5f379fb5c7efb7dbd40c40eee014325fa3fcc637
AceSoL-ls <eyris97@gmail.com>
2026-02-13 20:37:10 +0200

    Swing updates

--- GridLayout/src/MyFrame.java
+++ GridLayout/src/MyFrame.java
@@ -2,8 +2,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyFrame extends JFrame {
-   public MyFrame(){
-       this.setSize(500,500);
-       this.setLayout(new GridLayout(3,3));
+
+    JPanel panel = new JPanel();
+
+    public MyFrame(){
+        this.setSize(500,500);
+        this.setLayout(new GridLayout(3,3));
+
+
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
+        this.add(panel);
+        this.setVisible(true);
     }
 }
