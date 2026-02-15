18b2e10ac8427cd0c4c9636b552e84aa040da3a1
AceSoL-ls <eyris97@gmail.com>
2026-02-15 21:29:21 +0200

    Added into the Frame a JLayeredPane

--- JLayeredPane/src/MyFrame.java
+++ JLayeredPane/src/MyFrame.java
@@ -2,4 +2,20 @@ import javax.swing.*;
 
 public class MyFrame extends JFrame {
 
+    JLayeredPane layeredPane = new JLayeredPane();
+
+
+    public MyFrame(){
+        this.setTitle("Crazy app");
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        layeredPane.setBounds(0,0,500,500);
+
+
+
+        this.add(layeredPane);
+        this.setVisible(true);
+    }
 }
