c6fa4065487e66b591a042bcf005bd2ab1020f9c
AceSoL-ls <eyris97@gmail.com>
2026-02-18 23:05:11 +0200

    Fixed opening many tabs after clicking new Window

--- Swing_Open_New_Window/src/LaunchPage.java
+++ Swing_Open_New_Window/src/LaunchPage.java
@@ -24,7 +24,7 @@ public class LaunchPage implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource()==button) {
-
+            frame.dispose();
             NewWindow myWindow = new NewWindow();
         }
 
